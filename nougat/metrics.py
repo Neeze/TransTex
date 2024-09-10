18e09d94fef0c86eb00f8a28b2f99c24a156ca30
Neeze <phanvanhainam.2019@gmail.com>
2024-09-10 22:50:35 +0700

    Refactor compute_metrics to use nltk.translate.meteor_score instead of nltk.translate.meteor

--- nougat/metrics.py
+++ nougat/metrics.py
@@ -32,10 +32,10 @@ def compute_metrics(pred, gt, minlen=4):
     reference = gt.split()
     hypothesis = pred.split()
     metrics["bleu"] = nltk.translate.bleu([reference], hypothesis)
-    try:
-        metrics["meteor"] = nltk.translate.meteor_score([reference], hypothesis)
-    except LookupError:
-        metrics["meteor"] = np.nan
+    # try:
+    #     metrics["meteor"] = nltk.translate.meteor_score([reference], hypothesis)
+    # except LookupError:
+    metrics["meteor"] = np.nan
     reference = set(reference)
     hypothesis = set(hypothesis)
     metrics["precision"] = nltk.scores.precision(reference, hypothesis)
